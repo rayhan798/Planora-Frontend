ad1dd11f95ce7627b3ca016d9609776e746fa6f4
Rayhan <mdrayhan0474@gmail.com>
2026-04-24 23:36:12 +0600

    chore: update project configuration and dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,43 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // reactCompiler:true,
+
+  // // better auth proxy
+  // async rewrites() {
+
+  //   return [
+  //     {
+  //       // Explicitly map auth requests
+  //       source: "/api/auth/:path*",
+  //       destination: process.env.NEXT_PUBLIC_BACKEND_URL + "/api/auth/:path*",
+  //     },
+
+  //   {
+  //       // Explicitly map v1 API requests
+  //       source: "/api/v1/:path*",
+  //       destination: process.env.NEXT_PUBLIC_BACKEND_URL + "/api/v1/:path*",
+  //     },
+  //   ];
+  // };
+
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "10mb",
+    },
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
